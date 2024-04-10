c7a72e5065c3c1fe5cf854197fe51185ce3ec6e3
birju22112002 <69200127+brijesh6353@users.noreply.github.com>
2024-04-10 14:34:25 +0530

    c

--- src/app/components/nav/AdminNav.js
+++ src/app/components/nav/AdminNav.js
@@ -1,13 +1,11 @@
 /** @format */
 
 import React, { useState, useContext, useEffect } from "react";
-import { Menu, Button, Layout } from "antd";
+import { Menu, Layout } from "antd";
 import Link from "next/link";
 import { ThemeContext } from "../../context/ThemeContext";
 import { useWindowWidth } from "@react-hook/window-size";
 import {
-  PieChartOutlined,
-  MailOutlined,
   PushpinOutlined,
   CameraOutlined,
   UserSwitchOutlined,
@@ -40,7 +38,9 @@ const AdminNav = () => {
     }
   }, [onlyWidth < 800]);
 
-  const activeName = (name) => `${current === name && "active"}`;
+  const handleClick = (e) => {
+    setCurrent(e.key);
+  };
 
   return (
     <Sider
@@ -56,33 +56,39 @@ const AdminNav = () => {
         }}
         // defaultSelectedKeys={["1"]}
         defaultOpenKeys={["2", "6", "10"]}
+        selectedKeys={[current]}
         mode='inline'
         inlineCollapsed={collapsed}
-        theme={theme === "dark" ? "dark" : "light"}>
+        theme={theme === "dark" ? "dark" : "light"}
+        onClick={handleClick}>
         <Menu.Item key='1' icon={<SettingOutlined />}>
-          <Link href='/pages/admin' className={activeName("/pages/admin")}>
+          <Link
+            href='/pages/admin'
+            className={current === "/pages/admin" ? "active" : null}>
             Dashboard
           </Link>
         </Menu.Item>
 
         {/* posts */}
         <SubMenu key='2' icon={<PushpinOutlined />} title='Posts'>
-          <Menu.Item key='3'>
+          <Menu.Item key='/admin/posts/new'>
             <Link
               href='/admin/posts/new'
-              className={activeName("/pages/posts1")}>
+              className={current === "/admin/posts/new" ? "active" : null}>
               All Posts
             </Link>
           </Menu.Item>
-          <Menu.Item key='4'>
-            <Link href='/pages/posts' className={activeName("/pages/posts")}>
+          <Menu.Item key='/pages/posts'>
+            <Link
+              href='/pages/posts'
+              className={current === "/pages/posts" ? "active" : null}>
               Add New
             </Link>
           </Menu.Item>
-          <Menu.Item key='5'>
+          <Menu.Item key='/pages/categories'>
             <Link
               href='/pages/categories'
-              className={activeName("/pages/posts1")}>
+              className={current === "/pages/categories" ? "active" : null}>
               Categories
             </Link>
           </Menu.Item>
@@ -90,55 +96,63 @@ const AdminNav = () => {
 
         {/* library */}
         <SubMenu key='6' icon={<CameraOutlined />} title='Media'>
-          <Menu.Item key='7'>
+          <Menu.Item key='/admin/media/library'>
             <Link
               href='/admin/media/library'
-              className={activeName("/pages/posts1")}>
+              className={current === "/admin/media/library" ? "active" : null}>
               Library
             </Link>
           </Menu.Item>
-          <Menu.Item key='8'>
+          <Menu.Item key='/admin/media/new'>
             <Link
               href='/admin/media/new'
-              className={activeName("/pages/posts1")}>
+              className={current === "/admin/media/new" ? "active" : null}>
               Add New
             </Link>
           </Menu.Item>
         </SubMenu>
 
         {/* comments */}
-        <Menu.Item key='9' icon={<CommentOutlined />}>
-          <Link href='/admin/comments' className={activeName("/pages/posts1")}>
+        <Menu.Item key='/admin/comments' icon={<CommentOutlined />}>
+          <Link
+            href='/admin/comments'
+            className={current === "/admin/comments" ? "active" : null}>
             Comments
           </Link>
         </Menu.Item>
 
         {/* users */}
         <SubMenu key='10' icon={<UserSwitchOutlined />} title='Users'>
-          <Menu.Item key='11'>
-            <Link href='/admin/users' className={activeName("/pages/posts1")}>
+          <Menu.Item key='/admin/users'>
+            <Link
+              href='/admin/users'
+              className={current === "/admin/users" ? "active" : null}>
               All Users
             </Link>
           </Menu.Item>
-          <Menu.Item key='12'>
+          <Menu.Item key='/admin/users/new'>
             <Link
               href='/admin/users/new'
-              className={activeName("/pages/posts1")}>
+              className={current === "/admin/users/new" ? "active" : null}>
               Add New
             </Link>
           </Menu.Item>
         </SubMenu>
 
         {/* profile */}
-        <Menu.Item key='13' icon={<UserOutlined />}>
-          <Link href='/admin/userid' className={activeName("/pages/posts1")}>
+        <Menu.Item key='/admin/userid' icon={<UserOutlined />}>
+          <Link
+            href='/admin/userid'
+            className={current === "/admin/userid" ? "active" : null}>
             Profile
           </Link>
         </Menu.Item>
 
         {/* Customize */}
-        <Menu.Item key='14' icon={<BgColorsOutlined />}>
-          <Link href='/admin/customize' className={activeName("/pages/posts1")}>
+        <Menu.Item key='/admin/customize' icon={<BgColorsOutlined />}>
+          <Link
+            href='/admin/customize'
+            className={current === "/admin/customize" ? "active" : null}>
             Customize
           </Link>
         </Menu.Item>
